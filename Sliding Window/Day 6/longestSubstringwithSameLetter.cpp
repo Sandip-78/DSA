7c75f81eec6df92df0aacf7b18af7307f67cae3d
Sandip Parmar <sandipparmar7813@gmail.com>
2026-01-05 23:57:12 +0530

    sw6

---
+++ Sliding Window/Day 6/longestSubstringwithSameLetter.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int find(int count[]){
+    int maxCount = 0;
+    for(int i=0;i<256;i++){
+        maxCount = max(maxCount,count[i]);
+    }
+    return maxCount;
+}   
+
+int main(){
+
+    string s = "AABABBA";
+    int k = 2;
+    int low = 0, high = 0,res = 0;
+    int count[256] = {0};
+
+    for(high=0;high<s.size();high++){
+        count[s[high]]++;
+        int len = high-low+1;
+        int maxCount = find(count);
+        int diff = len - maxCount;
+
+        while(diff>k){
+            count[s[low]]--;
+            low++;
+            len = high - low +1;
+            maxCount = find(count);
+            diff = len - maxCount;
+        }
+        len = high - low +1;
+        res = max(res,len);
+    }
+       cout<<res;
+    return 0;
+}
