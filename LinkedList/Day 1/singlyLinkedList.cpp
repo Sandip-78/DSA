76673b8a95b780af1a3e37160bab3c57912f52f0
Sandip Parmar <sandipparmar7813@gmail.com>
2026-01-24 00:10:41 +0530

    push_front and push_bacl

---
+++ LinkedList/Day 1/singlyLinkedList.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public : 
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+    Node* head;
+    Node* tail;
+
+public :
+    List(){
+        head=tail=NULL;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        }else{
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val);
+        
+        // Node* temp = head;
+        // if(head == NULL){
+        //     head = newNode;
+        // }
+        // while(temp->next!= NULL){
+        //     temp = temp->next;
+        // }
+        // temp->next = newNode;
+        // newNode->next = NULL;
+
+        if(head==NULL){
+            head=tail=newNode;
+        }else{
+            tail->next=newNode;
+            tail=newNode;
+        }
+    }
+
+    void print(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+};
+
+int main(){
+
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_back(4);
+    ll.print();
+    return 0;
+}
