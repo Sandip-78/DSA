b60dadac4289f2c7fadf58798f5684d726f7ebf1
Sandip Parmar <sandipparmar7813@gmail.com>
2026-01-24 23:36:40 +0530

    complete

--- LinkedList/Day 1/singlyLinkedList.cpp
+++ LinkedList/Day 1/singlyLinkedList.cpp
@@ -53,6 +53,71 @@ public :
         }
     }
 
+    void pop_front(){
+        if(head == NULL){
+            cout<<"LL is empty";
+            return ;
+        }else{
+            Node* temp = head;
+            head = head->next;
+            temp->next=NULL;
+            delete temp;
+        }
+    }
+
+    void pop_back(){
+        if(head==NULL){
+            cout<<"LL is empty";
+            return ;
+        }else{
+            Node* temp = head;
+            while(temp->next != tail){
+                temp = temp->next; 
+            }
+            temp->next = NULL;
+            delete tail;
+            tail = temp;
+        }
+    }
+
+    void insert(int val,int pos){
+        if(pos < 0){
+            cout<<"Invalid Pos\n";
+            return;
+        }
+        else if(pos == 0){
+            push_front(val);
+            return;
+        }
+        else{
+            Node* newNode = new Node(val);
+            Node* temp = head;
+            for(int i=0;i<pos-1;i++){
+                temp = temp->next;
+            }
+
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+        
+    }
+
+    int search(int key){
+        Node* temp = head;
+        int idx = 0;
+
+        while(temp !=NULL){
+        if(temp->data == key){
+            return idx;
+        }
+        temp = temp->next;
+        idx++;
+        }
+
+        return -1;
+        
+    }
+
     void print(){
         Node* temp = head;
         while(temp != NULL){
@@ -65,10 +130,16 @@ public :
 int main(){
 
     List ll;
-    ll.push_front(1);
-    ll.push_front(2);
     ll.push_front(3);
-    ll.push_back(4);
+    ll.push_front(2);
+    ll.push_front(1);
     ll.print();
+    cout<<endl;
+    ll.insert(4,1);
+    ll.print();
+    cout<<endl;
+    cout<<ll.search(2);
+
+    
     return 0;
 }
