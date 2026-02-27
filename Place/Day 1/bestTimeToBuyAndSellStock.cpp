ecff6cd1f0fd56d4f4ae6d3690d3764b3fec663a
Sandip Parmar <sandipparmar7813@gmail.com>
2026-02-27 23:20:04 +0530

    Day 1

---
+++ Place/Day 1/bestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int profit = 0;
+       int bestBuy = prices[0];
+       for(int i=1;i<prices.size();i++){
+            if(prices[i]>bestBuy){
+                profit = max (profit, prices[i]-bestBuy);
+            }
+            bestBuy = min (bestBuy,prices[i]);
+       }
+
+       return profit;
+    }
+};
