67f8776312977743812c80b8000a6eeef78a4258
Sandip Parmar <sandipparmar7813@gmail.com>
2026-04-14 14:37:51 +0530

    day 2

---
+++ Arrays/Day 2/intersectionOfTwoArrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& a, vector<int>& b) {
+        int i=0;
+        int j=0;
+        vector<int> ans;
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        while(i<a.size() && j<b.size()){
+            if(a[i] < b[j]){
+                i++;
+            }else if(a[i]>b[j]){
+                j++;
+            }else{
+                ans.push_back(a[i]);
+                i++;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
